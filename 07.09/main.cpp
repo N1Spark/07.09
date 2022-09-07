8dae2b96e4de89dc737a980c4d4567d04a4e7e58
Dmytro <dimkavkru@gmail.com>
2022-09-07 13:01:49 +0300

    Добавил конструктор по умолч. и конструктор с параметроми

--- 07.09/main.cpp
+++ 07.09/main.cpp
@@ -34,12 +34,21 @@ public:
 	}
 };
 
-class Stident
+class Student
 {
 	string name;
 	int age;
 public:
+	Student()
+	{
 
+	}
+	Student(string n, int a)
+	{
+		name = n;
+		age = a;
+
+	}
 };
 
 int main()
